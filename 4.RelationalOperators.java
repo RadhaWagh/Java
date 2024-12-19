2321b17ca1ab47f73edb6fb2b65df5f3f7ccb761
Radha Wagh <145145559+RadhaWagh@users.noreply.github.com>
2024-12-19 22:43:43 +0530

    Add files via upload

---
+++ 4.RelationalOperators.java
@@ -0,0 +1,13 @@
+class RelationalOperators
+{
+   public static void main(String[] args)
+    {
+        int x=6;
+        int y=4;
+
+        boolean result = x < y;
+
+        System.out.print(result);
+    }
+
+}

3b5dc25bd8fa4febc4a2f07ac488bbbcd7ac9b62
Radha Wagh <145145559+RadhaWagh@users.noreply.github.com>
2024-12-27 22:22:16 +0530

    Add files via upload

---
+++ 17.Array.java
@@ -0,0 +1,17 @@
+public class Array
+{
+    public static void main(String[] args) {
+        int nums[]=new int[4];
+        nums[0]=4;
+        nums[1]=3;
+        nums[2]=8;
+        nums[3]=9;
+
+        for(int i=0;i<4;i++)
+        {
+            System.out.println(nums[i]);
+        }
+
+    }
+
+}
